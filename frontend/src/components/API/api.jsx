ff3661c431b106fe2f624b251591586278dc7f21
Goldsap <92326203+GoldySap@users.noreply.github.com>
2026-03-18 09:25:57 +0100

    api and cors fix

--- frontend/src/components/API/api.jsx
+++ frontend/src/components/API/api.jsx
@@ -1,10 +1,13 @@
 export async function api(endpoint, options = {}) {
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const url = `${backendUrl}${endpoint.startsWith("/") ? endpoint : "/" + endpoint}`;
-  
+
   const res = await fetch(url, {
     credentials: "include",
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      ...(options.body && { "Content-Type": "application/json" }),
+      ...options.headers,
+    },
     ...options,
   });
 
