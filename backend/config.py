9842a42a398b1e990a127eb1ad442bf2eab8dcf8
Goldsap <92326203+GoldySap@users.noreply.github.com>
2026-04-15 11:04:30 +0200

    l

--- backend/config.py
+++ backend/config.py
@@ -29,12 +29,12 @@ class Config:
     JWT_TOKEN_LOCATION = ["cookies"]
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=7)
-    JWT_COOKIE_SECURE = False if os.environ.get("FLASK_ENV") == "production" else False
-    JWT_COOKIE_SAMESITE = "Lax" if os.environ.get("FLASK_ENV") == "production" else "Lax"
-    JWT_COOKIE_HTTPONLY = False
+    JWT_COOKIE_SECURE = True if os.environ.get("FLASK_ENV") == "production" else False
+    JWT_COOKIE_SAMESITE = "None" if os.environ.get("FLASK_ENV") == "production" else "Lax"
+    JWT_COOKIE_HTTPONLY = True
     JWT_COOKIE_CSRF_PROTECT = True
     JWT_CSRF_IN_COOKIES = True
-    JWT_CSRF_CHECK_FORM = False
+    JWT_CSRF_CHECK_FORM = True
     JWT_ACCESS_CSRF_COOKIE_NAME = "csrf_access_token"
     JWT_REFRESH_CSRF_COOKIE_NAME = "csrf_refresh_token"
     JWT_ACCESS_CSRF_HEADER_NAME = "X-CSRF-TOKEN"
